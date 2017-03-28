3a5a44e38a4dcc7126b855e1891285190a09ac13
itggot-Harry-Norrman <harry.norrman@itggot.se>
2017-03-28 11:42:03 +0200

    Färdig med ovn.html

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,21 @@
 function menu() {
   document.querySelector(".menu").classList.add("anim")
+  document.querySelector(".shade").classList.add("shd_ani")
+  document.querySelector(".shade").classList.remove("left")
 }
-function achivement() {
-  document.querySelector("")
+function achievement() {
+  document.querySelector(".achieve").classList.remove("display_n")
+    document.querySelector(".res_js").classList.remove("display_n")
+}
+function removeAch() {
+  document.querySelector(".achieve").classList.add("display_n")
+  document.querySelector(".res_js").classList.add("display_n")
+}
+function remMen() {
+  document.querySelector(".menu").classList.remove("anim")
+  document.querySelector(".shade").classList.remove("shd_ani")
+  document.querySelector(".shade").classList.add("left")
+}
+function notification() {
+  document.querySelector("").classList.add("")
 }
